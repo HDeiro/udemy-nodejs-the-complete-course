8b95cf10c5676f18bfe050c32ac645dca708664e
Hugo Deiró <hugo.souza@wexinc.com>
2020-06-03 10:05:25 -0300

    Adding Redirection

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const http   = require('http');
+const fs     = require('fs');
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const { url, method } = req;
+
     res.setHeader('Content-Type', 'text/html');
-    res.write('<h1>Response!</h1>');
+
+    if (url === '/') {
+        res.write(`
+            <form action="/message" method="POST">
+                <input type="text" name="message">
+                <button type="submit">Send</button>
+            </form>`
+        );
+    } else if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'Dummy!');
+        res.statusCode = 302;
+        res.setHeader('Location', '/')
+    } else {
+        res.write(`<h1>It works!</h1>`);
+    }
+
     res.end();
 });
 
