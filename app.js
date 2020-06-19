d534169d183739cc487b4dff20c4d16e7f04cb7c
Hugo Deiró <hugo.souza@wexinc.com>
2020-06-19 06:20:49 -0300

    404 Route

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const adminShop   = require('./routes/shop');
 
 // Register a middleware to handle the body parameters as URL Encoded
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// App middlewares
 app.use(adminRouter);
 app.use(adminShop);
+
+app.use((req, res) => {
+    res.status(404).send('Page not found');
+});
 
 app.listen(3000);
