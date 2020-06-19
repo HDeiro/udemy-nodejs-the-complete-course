92298157949d5b4a48f496a7c0dd6f330698431a
Hugo Deiró <hugo.souza@wexinc.com>
2020-06-19 07:05:06 -0300

    Serving HTML files

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
-const app         = express();
+const path        = require('path');
 const adminRouter = require('./routes/admin');
 const adminShop   = require('./routes/shop');
+
+const app = express();
 
 // Register a middleware to handle the body parameters as URL Encoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,7 +14,7 @@ app.use('/admin', adminRouter);
 app.use(adminShop);
 
 app.use((req, res) => {
-    res.status(404).send('Page not found');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
