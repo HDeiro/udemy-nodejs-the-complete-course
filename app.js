b7376f3ac5ca0ba83c97b5384fa713c8e6934bc1
Hugo Deiró <hugo.souza@wexinc.com>
2020-06-19 06:18:12 -0300

    Separating in different route files

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
-const express    = require('express');
-const bodyParser = require('body-parser');
-const app        = express();
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const app         = express();
+const adminRouter = require('./routes/admin');
+const adminShop   = require('./routes/shop');
 
 // Register a middleware to handle the body parameters as URL Encoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// General Middlewares
-
-app.use('/add-product', (req, res) => {
-    res.send(`
-        <form action="/product" method="POST">
-            <input type="text" name="title">
-            <button type="submit">Add Product</button>
-        </form>
-    `)
-});
-
-app.use('/product', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res) => {
-    res.send('<h1>Hello from Express</h1>')
-});
+app.use(adminRouter);
+app.use(adminShop);
 
 app.listen(3000);
