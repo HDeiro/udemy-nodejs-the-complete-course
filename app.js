a842dec7dcfc6973fd38ca1ae092a6327579dbfb
Hugo Deiró <hugo.souza@wexinc.com>
2020-06-22 07:34:20 -0300

    Creating Path Utilitary

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser  = require('body-parser');
 const path        = require('path');
 const adminRouter = require('./routes/admin');
 const adminShop   = require('./routes/shop');
+const rootDir     = require('./utils/path');
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.use('/admin', adminRouter);
 app.use(adminShop);
 
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
